8f7eb62296d2357e7dacdc69e18ea1a9c1562cd4
Hasan Laheri <laherihasan@gmail.com>
2025-10-23 16:26:22 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+     version="0.1.0",
+     author="Hasan Qaid Joher Laheri",
+     author_email="Laherihasan@gmail.com",
+     packages=find_packages(),
+     install_requires = []
+)
